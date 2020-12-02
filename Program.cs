1a0cc5fb82fe270eeb9181b94550d69c2a63998f
Mark Pritchett <Mark.Pritchett@anchor.org.uk>
2020-12-02 16:57:11 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+
+var conversionOptions = JsonSerializer.Deserialize<ConversionOption>(File.ReadAllText(args[0]));
+
+foreach (var extensionGrouping in conversionOptions.Options.GroupBy(x => x.Extension))
+{
+    foreach (var file in Directory.EnumerateFiles(conversionOptions.SourceDir, $"*{extensionGrouping.Key}", SearchOption.AllDirectories))
+    {
+        foreach (var option in extensionGrouping)
+        {
+            var contents = File.ReadAllText(file, Encoding.UTF8);
+
+            if (option.IsRegex)
+            {
+                contents = Regex.Replace(contents, option.Find, option.ReplaceWith);
+            }
+            else
+            {
+                contents = contents.Replace(option.Find, option.ReplaceWith);
+            }
+
+            File.WriteAllText(file, contents, Encoding.UTF8);
+        }
+    }
+}
+
+record ConversionOption(string SourceDir, Option[] Options);
+record Option(string Find, string ReplaceWith, string Extension, bool IsRegex);
---
+++ README.md
@@ -0,0 +1,34 @@
+# Conversion Utility
+
+## Overview
+
+A command line application to find and replace text (plain or regex based).
+
+The `SourceDir` along with the find and replace options are configured via a `JSON` file (example below)
+
+## Use Cases
+
+To help migrating to a new version of a library, (i.e., migrating from Bootstrap v3 to v4)
+
+It's intended for use as a starting point for any migrations. I don't expect it will cover all your needs.
+
+## Usage 
+
+```
+ConversionUtility.exe C:\temp\conversion-options.json
+```
+
+**WARNING: Run at your own risk and take precautions for loss of information / corruption through source control or backups**
+
+## Sample conversion options file contents
+
+``` JSON
+{
+	"SourceDir": "C:\\src\\MyAppLocation\\",
+	"Options":
+	[
+		{ "Find": "pull-left", "ReplaceWith": "float-left", "Extension": ".cshtml" },
+		{ "Find": "pull-right", "ReplaceWith": "pull-left", "Extension": ".cshtml" }
+	]
+}
+```
